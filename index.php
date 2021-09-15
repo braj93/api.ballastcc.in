f92e77d78dd6daf336e3401abebf2219303b2379
tech939 <techgyan939@gmail.com>
2021-09-16 04:17:24 +0530

    reinitialize project

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 /**
  * CodeIgniter
  *
@@ -54,25 +53,8 @@ header("Access-Control-Allow-Origin: *");
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	
-	if (isset($_GET['sinfo'])) {
-		phpinfo();die();
-	}
-	switch ($_SERVER['SERVER_NAME']) {
-	case 'www.tikisites.com':
-		define('ENVIRONMENT', "production");
-		break;
-	case 'staging.tikisites.com':
-		define('ENVIRONMENT', "staging");
-		break;
-	case 'dev.tikisites.com':
-		define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-		break;
-	default:
-		define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'local');
-		break;
-	}
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -81,63 +63,31 @@ header("Access-Control-Allow-Origin: *");
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-// switch (ENVIRONMENT)
-// {
-// 	case 'development':
-// 		error_reporting(-1);
-// 		ini_set('display_errors', 1);
-// 	break;
-
-// 	case 'testing':
-// 	case 'production':
-// 		ini_set('display_errors', 0);
-// 		if (version_compare(PHP_VERSION, '5.3', '>='))
-// 		{
-// 			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-// 		}
-// 		else
-// 		{
-// 			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-// 		}
-// 	break;
-
-// 	default:
-// 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-// 		echo 'The application environment is not set correctly.';
-// 		exit(1); // EXIT_ERROR
-// }
-
-// case 'production':
-// 		error_reporting(-1);
-// 		ini_set('display_errors', 1);
-// 		break;
-// 	case 'staging':
-// 		error_reporting(0);
-// 		ini_set('display_errors', 0);
-// 		break;
-
-switch (ENVIRONMENT) {
-	case 'production':
+switch (ENVIRONMENT)
+{
+	case 'development':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
-		break;
-	case 'staging':
-		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-		break;
-	case 'development':
-		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-		break;
-	case 'local':
-		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-		break;
+	break;
+
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
+
 	default:
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'The application environment is not set correctly.';
 		exit(1); // EXIT_ERROR
-	}
+}
 
 /*
  *---------------------------------------------------------------
