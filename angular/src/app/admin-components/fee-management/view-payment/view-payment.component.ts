02632e6cd76c36a5221708479fc666876afc334e
tech939 <techgyan939@gmail.com>
2021-11-24 21:08:30 +0530

    payment slip print feature add

--- angular/src/app/admin-components/fee-management/view-payment/view-payment.component.ts
+++ angular/src/app/admin-components/fee-management/view-payment/view-payment.component.ts
@@ -9,6 +9,7 @@ import { AdminService } from 'src/app/core';
 })
 export class ViewPaymentComponent implements OnInit {
   public print_data:any="";
+  public name ="braj yadav";
   constructor(
     public route: ActivatedRoute,
     public adminService: AdminService,
@@ -18,9 +19,41 @@ export class ViewPaymentComponent implements OnInit {
     this.print_data =document.getElementById("free-receipt-print");
   }
   
-  printPage() {
-    
-    window.print();
-  }
+  // printPage(divName:any) {
+  //   // var divContents = document.getElementById(divName).innerHTML;
+  //   let myContainer = document.getElementById(divName) as HTMLInputElement;
+   
+  // }
 
+  printPage(divName:any): void {
+    let printContents, popupWin;
+    printContents = document.getElementById(divName)?.innerHTML;
+    console.log(printContents);
+    // printContents = document.getElementById(divName) as HTMLInputElement;
+    popupWin = window.open('', '_blank');
+    popupWin?.document.open();
+    popupWin?.document.write(`
+      <html>
+        <head>
+          <title></title>
+          <style>
+          // table, th, td {
+          //   border: 1px solid black;
+          //   border-collapse: collapse;
+          // }
+          </style>
+        </head>
+    <!---<body onload="window.print();window.close()"> --->
+    <body onload="window.print();window.close()">
+    <div>
+    ${printContents}
+    <div style="width: 100%; border: 1px dashed black; margin:30px 0px;"></div>
+    ${printContents}
+    </div>
+    </body>
+      </html>`
+    );
+    popupWin?.document.close();
+
+}
 }
