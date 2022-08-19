2f45de4c54e0769253492809b15c7ac0662a7fb2
tech939 <techgyan939@gmail.com>
2022-08-19 23:13:04 +0530

    update acess file

--- application/config/rest.php
+++ application/config/rest.php
@@ -358,7 +358,7 @@ $config['rest_keys_table'] = 'user_login_sessions';
 |   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
  */
-$config['rest_enable_keys'] = TRUE;
+$config['rest_enable_keys'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
